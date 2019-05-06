329df4d7095b3898b477e990eae878433598b480
yuanshuai <yuanshuai@localhost.localdomain>
2019-05-06 07:53:41 +0000

    ceshi

--- app/Http/Controllers/Wechat/WechatController.php
+++ app/Http/Controllers/Wechat/WechatController.php
@@ -27,10 +27,41 @@ class WechatController extends Controller
         $Event = $obj->Event;//获取时间类型
         if($MsgType=="event"){
             if($Event=="subscribe"){
-                $text="欢迎关注老袁头的微信";
+                $text="欢迎关注老袁头的微信,\n
+                 回复1查看老袁头班级所有人名单,\n
+                 回复2随机查看一位班级人姓名,\n
+                 回复天气查看当地天气情况,\n";
                 //用户关注回复消息
                 $xml=$this->ReturnText($FromUserName,$ToUserName,$text);
-                echo $xml;
+                echo $xml;exit;
+            }
+        }elseif($MsgType=="text"){
+            $Content = $obj->Content;//获取文字内容
+            $arr=[
+                "杨","添","雯","赵","泽","宇","张","豪","袁","帅"
+            ];
+            if(strpos($Content,"+天气")){
+                //回复天气消息
+                $city=mb_substr($Content,0,2);//截取城市名称
+                $url="https://free-api.heweather.net/s6/weather/now?key=HE1904161039381186&location=$city";//调接口
+                $json=file_get_contents($url);//获取数据
+                $arr=json_decode($json,true);//转换数据类型
+                var_dump($arr);exit;
+            }elseif($Content=="1"){
+                //回复班级全部人员
+                $text=implode(",",$arr);
+                $xml=$this->ReturnText($FromUserName,$ToUserName,$text);
+                echo $xml;exit;
+            }elseif($Content=="2"){
+                //随机回复一个班级人员
+                $count=count($arr);
+                $str=rand(0,$count-1);
+                $text=$arr[$str];
+                $xml=$this->ReturnText($FromUserName,$ToUserName,$text);
+                echo $xml;exit;
+            }else{
+                //返回无结果
+                $text="抱歉，目前暂时无法为您找到相关的服务";
             }
         }
         
