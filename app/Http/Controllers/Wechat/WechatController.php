a5c55a4a008ec3c0d851c035805f0e0ac60c47e8
yuanshuai <yuanshuai@localhost.localdomain>
2019-05-06 03:23:54 +0000

    关注回复文本消息

--- app/Http/Controllers/Wechat/WechatController.php
+++ app/Http/Controllers/Wechat/WechatController.php
@@ -19,6 +19,32 @@ class WechatController extends Controller
         $time = date('Y-m-d H:i:s') . "\n" . $data . "\n<<<<<<<";//存入时间
         file_put_contents("logs/wx_event.log", $time, FILE_APPEND);//存到public日志文件
         $obj = simplexml_load_string($data);//将xml数据转换成对象格式的数据
+
+        $ToUserName = $obj->ToUserName;//获取开发者微信号
+        $FromUserName = $obj->FromUserName;//获取用户id（openid）
+        $CreateTime = $obj->CreateTime;//获取时间
+        $MsgType = $obj->MsgType;//获取数据类型
+        $Event = $obj->Event;//获取时间类型
+        if($MsgType=="event"){
+            if($Event=="subscribe"){
+                //用户关注回复消息
+                $xml='
+                    <xml>
+              <ToUserName><![CDATA[$FromUserName]]></ToUserName>
+              <FromUserName><![CDATA[$ToUserName]]></FromUserName>
+              <CreateTime>.time().</CreateTime>
+              <MsgType><![CDATA[text]]></MsgType>
+              <Content><![CDATA[欢迎订阅老袁头的公众号]]></Content>
+            <xml>';
+                echo $xml;
+            }
+        }else if($MsgType=="text"){
+            $Content = $obj->Content;//获取文本消息内容
+            if(strpos($Content,"+天气")){
+                //回复天气情况
+            }
+        }
+
         echo "SUCCESS";
     }
 }
