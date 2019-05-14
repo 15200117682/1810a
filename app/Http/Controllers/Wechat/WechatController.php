2025cf0b26870d4bf7091041271a9f0210b83dce
yuanshuai <yuanshuai@localhost.localdomain>
2019-05-14 01:30:15 +0000

    整合表白

--- app/Http/Controllers/Wechat/WechatController.php
+++ app/Http/Controllers/Wechat/WechatController.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers\Wechat;
 use App\Model\ImgModel;
 use App\Model\MenuModel;
 use App\Model\UsersModel;
+use App\Model\ActModel;
+use App\Model\LoveModel;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\Redis;
@@ -40,6 +42,24 @@ class WechatController extends Controller
                 $this->UserDb($FromUserName,$ToUserName,$data);//用户关注/*79*/
             }elseif($Event=="unsubscribe"){
                 $this->UserDbNo($FromUserName,$data);
+            }elseif($Event=="CLICK"){
+                $EventKey=$obj->EventKey;//获取事件信息
+                //查表白返回数据
+                if($EventKey=="select"){//查询菜单点击
+                    $text="请输入要查询的表白人名字";
+                    $name="查表白";
+                    $this->upEvent($FromUserName,$name);//存入事件
+                    $xml=$this->ReturnText($FromUserName,$ToUserName,$text);//返回给用户文字提示{调用的函数}
+                    echo $xml;exit;
+                }
+                //发表白返回数据
+                if($EventKey=="send"){//发送表白菜单点击
+                    $text="请输入要发送表白人的名字";
+                    $name="发表白";
+                    $this->upEvent($FromUserName,$name);//存入事件
+                    $xml=$this->ReturnText($FromUserName,$ToUserName,$text);//返回给用户文字提示{调用的函数}
+                    echo $xml;exit;
+                }
             }
         }elseif($MsgType=="image"){
             $data=ImgModel::orderByRaw("RAND()")->first();//随机查询一条数据
@@ -59,31 +79,111 @@ class WechatController extends Controller
 
         }elseif($MsgType=="text"){
             $Content = $obj->Content;//获取文字内容
-            $arr=[
-                "杨","添","雯","赵","泽","宇","张","豪","袁","帅"
-            ];//自定义数组
-            if(strpos($Content,"天气")){
-                //天气接口
-                $this->Weacher($FromUserName,$ToUserName,$Content);//天气接口/*117*/
-            }elseif($Content=="1"){
-                //回复班级全部人员
-                $text=implode(",",$arr);
-                $xml=$this->ReturnText($FromUserName,$ToUserName,$text);//回复消息
-                echo $xml;exit;
-            }elseif($Content=="2"){
-                //随机回复一个班级人员
-                $count=count($arr);
-                $str=rand(0,$count-1);
-                $text=$arr[$str];
-                $xml=$this->ReturnText($FromUserName,$ToUserName,$text);//回复消息
-                echo $xml;exit;
+            $data=$this->allEvent($FromUserName);//查看上一步事件
+
+            if($data && (time()-$data['act_time'] < 60)){
+                $this->biaobai($FromUserName,$ToUserName,$data,$Content);
             }else{
-                //图灵机器人在线聊天
-                $this->RoBot($FromUserName,$ToUserName,$Content);//机器人/*105*/
+                $this->text($FromUserName,$ToUserName,$Content);
             }
 
         }
         
+    }
+
+    public function biaobai($FromUserName,$ToUserName,$datainfo,$Content){
+        if($datainfo['act_name']=="发表白"){
+            $text="请填写要表白的内容";
+            $name="发内容";
+            $this->upEvent($FromUserName,$name);//存上一步事件
+            /*把表白的具体内容存入数据库*/
+            $arr=[
+                'love_openid'=>$FromUserName,
+                'love_name'=>$Content,
+                'love_content'=>"",
+                "love_time"=>time()
+            ];//先把被表白的人名字入库
+            $res=LoveModel::insertGetId($arr);//入库
+            /*把表白的具体内容存入数据库*/
+            if($res){
+                $xml=$this->ReturnText($FromUserName,$ToUserName,$text);//提示用户发送表白内容
+                echo $xml;exit;
+            }
+        }elseif($datainfo['act_name']=="发内容"){
+            $arr=[
+                "love_content"=>$Content
+            ];//存入表白的内容
+            /*把表白的内容入库，然后提示用户表白成功*/
+            $resdata=LoveModel::where(['love_openid'=>"$FromUserName"])->orderBy("love_id","desc")->first()->toArray();
+            $resdata=$resdata['love_id'];
+            $res=LoveModel::where(['love_openid'=>$FromUserName])->where(["love_id"=>$resdata])->update($arr);//入库
+            /*把表白的内容入库，然后提示用户表白成功*/
+            if($res){
+                $text2="表白成功";
+                $xml=$this->ReturnText($FromUserName,$ToUserName,$text2);//提示用户发送表白内容
+                echo $xml;exit;
+            }
+        }elseif($datainfo['act_name']=="查表白"){
+            /************查询表白**************/
+            $res=LoveModel::where(['love_name'=>$Content])->first();//查询被表白的人
+            $res=json_decode($res,true);//转数组
+            $love_name=$res['love_name'];//被表白的人
+            $count=LoveModel::where(['love_name'=>$Content])->get()->count();//被表白了多少次
+            /*返回查询结果*/
+            if($res){
+                $str="表白："."$love_name"."\n"."被表白："."$count"."次";//回复给用户的消息
+                $xml=$this->ReturnText($FromUserName,$ToUserName,$str);//提示用户发送表白内容
+                echo $xml;exit;
+            }else{
+                $test="没人跟表白";
+                $xml=$this->ReturnText($FromUserName,$ToUserName,$test);//提示用户发送表白内容
+                echo $xml;exit;
+            }
+            /*返回查询结果*/
+            /************查询表白**************/
+        }
+    }
+
+    //记录上步事件
+    public function upEvent($FromUserName,$send){
+        $arr=[
+            "act_openid"=>$FromUserName,
+            "act_name"=>$send,
+            "act_time"=>time()
+        ];
+        ActModel::insertGetId($arr);
+    }
+
+    //查取上一步事件
+    public function allEvent($FromUserName){
+        $data=ActModel::where(["act_openid"=>$FromUserName])->orderBy('act_id','desc')->first();
+        return $data;
+    }
+
+    //自己写的文本回复
+    public function text($FromUserName,$ToUserName,$Content){
+        $arr=[
+            "杨","添","雯","赵","泽","宇","张","豪","袁","帅"
+        ];//自定义数组
+        if(strpos($Content,"天气")){
+            //天气接口
+            $this->Weacher($FromUserName,$ToUserName,$Content);//天气接口/*117*/
+        }elseif($Content=="1"){
+            //回复班级全部人员
+            $text=implode(",",$arr);
+            $xml=$this->ReturnText($FromUserName,$ToUserName,$text);//回复消息
+            echo $xml;exit;
+        }elseif($Content=="2"){
+            //随机回复一个班级人员
+            $count=count($arr);
+            $str=rand(0,$count-1);
+            $text=$arr[$str];
+            $xml=$this->ReturnText($FromUserName,$ToUserName,$text);//回复消息
+            echo $xml;exit;
+        }else{
+            //图灵机器人在线聊天
+            $this->RoBot($FromUserName,$ToUserName,$Content);//机器人/*105*/
+        }
     }
 
     /**
