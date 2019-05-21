c1bcbec7d0adbf6b4b78b88094c67d2facdfe391
yuanshuai <yuanshuai@localhost.localdomain>
2019-05-21 12:51:02 +0000

    授权静态页面

--- resources/views/ceshi/button.blade.php
+++ resources/views/ceshi/button.blade.php
@@ -1,41 +1,96 @@
 <!DOCTYPE html>
-<html lang="en">
+<html>
 <head>
     <meta charset="UTF-8">
-    <title>登录</title>
-    <link rel="stylesheet" href="/css/login.css">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <link rel="stylesheet" href="/css/style.css">
 </head>
 <body>
-<div class="login">
-    <div class="login_title">
-        <p><img src="/images/img/jinghui.png" style="width:80px;height:80px;margin-right:30px;"/>北京市公安局昌平分局网页授权平台</p>
-    </div>
-    <div class="login_main">
-        <div class="main_left"><img src="/images/img/you.jpg" alt=""></div>
-        <div class="main_right">
-            <div class="right_title">用户登录</div>
-            <form action="">
-                <div class="username">
-                    <img src="/images/img/username.png" alt="">
-                    <input type="text" placeholder="请输入用户名">
+<header>
+    <nav class="b_clear">
+        <div class="nav_logo l_float">
+            <img src="/images/imgs/logo.svg" alt="">
+        </div>
+        <div class="nav_link r_float">
+            <ul>
+                <li><a href="#">返回首页</a></li>
+                <li><a href="#">关于我们</a></li>
+                <li><a href="#">联系我们</a></li>
+
+            </ul>
+        </div>
+    </nav>
+</header>
+<div class="container">
+    <div class="login_body l_clear">
+        <div class="login_form l_float">
+            <div class="login_top">
+                <img src="/images/imgs/logo_z.svg" alt="" class="">
+
+                <div class="login_tags b_clear">
+                    <span class="top_tag l_float active" onClick="PwdLogin()">密码登录</span>
+                    <span class="top_tag r_float" onClick="QrcodeLogin()">扫码登录</span>
                 </div>
-                <div class="password">
-                    <img src="/images/img/password.png" alt="">
-                    <input type="text" placeholder="请输入密码">
+            </div>
+            <div class="login_con">
+                <form action="" method="POST">
+                    <div>
+                        <label for="user_name">用户名</label>
+                        <input type="text" name="" id="user_name" placeholder="账号/手机号/邮箱">
+                        <img src="/images/imgs/icons/user.svg">
+                        <p class="tips hidden">请检查您的账号</p>
+                    </div>
+                    <div>
+                        <label for="user_pwd">密码</label>
+                        <input type="password" name="" id="user_pwd" placeholder="请输入账户密码">
+                        <img src="/images/imgs/icons/lock.svg">
+                        <p class="tips hidden">请检查您的密码</p>
+                    </div>
+                    <div class="b_clear">
+                        <label for="auth_code" class="b_clear">验证码</label>
+                        <input type="text" name="" id="auth_code" placeholder="" class="l_float" maxlength="6">
+
+                        <button class="auth_code l_float">获取验证码</button>
+                        <img src="/images/imgs/icons/auth_code.svg">
+                        <p class="tips hidden">验证码错误</p>
+
+                    </div>
+                    <div class="b_clear submit">
+
+                        <button type="submit">登&nbsp;&nbsp;录</button>
+                        <a href="#" class="r_float">忘记密码？</a>
+                        <p class="tips hidden">登录失败，请检查您的账户与密码</p>
+                    </div>
+                </form>
+            </div>
+            <div class="login_con hidden">
+                <div class="qr_code">
+                    <img src="/images/imgs/qr.png" alt="">
+                    <p>请使用微信扫码登录<br>仅支持已绑定微信的账户进行快速登录</p>
                 </div>
 
-                <div class="yes_login"><a href="">登&nbsp;&nbsp;&nbsp;&nbsp;录</a></div>
-                <center><p style="margin-top:20px;font-size:12px;color:#FA6F03;">登录遇到问题，请联系管理员</p></center>
-            </form>
+            </div>
+            <div class="login_otherAccount">
+                <span>第三方登录</span>
+                <a href="http://"><img src="/images/imgs/icons/wechat.svg" alt="微信登录"></a>
+                <a href="http://"><img src="/images/imgs/icons/weibo.svg" alt="微博登录"></a>
+                <a href=""><img src="/images/imgs/icons/qq.svg" alt="QQ登录"></a>
+            </div>
+
+        </div>
+        <div class="login_ad l_float" id="AdImg">
+            <img src="/images/imgs/timg.jpg" alt="">
+            <a href="">查看详情</a>
         </div>
     </div>
-    <div class="login_footer">
-        <p class="name">版权所有：北京市昌平区沙河分局</p>
-        <p>建议浏览器：IE8及以上、360、谷歌、Firefox v22</p>
-        <p>技术支持：北京市公管局科技有限公司 &nbsp;&nbsp;联系电话：022-xxxxxx  更多模板：<a href="http://www.mycodes.net/" target="_blank">源码之家</a></p>
-
+    <div class="footer">
+        <p>Copyright © 2013-2018  <a href="#">赫伟创意星空</a></p>
+        <a href="#" target="_blank"><img src="/images/imgs/icons/icp_record_filing.svg" alt="工信部备案">蒙ICP备15000557号</a>更多模板：<a href="http://www.mycodes.net/" target="_blank">源码之家</a>
     </div>
 </div>
 
+<script src="/js/login.js"></script>
 </body>
 </html>
