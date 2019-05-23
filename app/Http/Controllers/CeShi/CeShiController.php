fc3ad7bd19038684e76ec50d8c5b22353a561b86
yuanshuai <yuanshuai@localhost.localdomain>
2019-05-23 08:16:25 +0000

    jasdlf

--- app/Http/Controllers/CeShi/CeShiController.php
+++ app/Http/Controllers/CeShi/CeShiController.php
@@ -190,12 +190,12 @@ class CeShiController extends Controller
         $data=UsersModel::where(['status'=>1])->pluck("openid")->toArray();
         $access=getAccessToken();
         $url="https://api.weixin.qq.com/cgi-bin/message/mass/send?access_token=$access";
-        $goods=GoodsModel::where(['status'=>1])->orderByRaw("RAND()")->first()->toArray();//随机查询一条数据
+        $goods=GoodsModel::where(['is_up'=>1])->orderByRaw("RAND()")->first()->toArray();//随机查询一条数据
         $datainfo=[
             "touser"=>$data,
             "msgtype"=>"text",
             "text"=>[
-                "content"=>"$goods"
+                "content"=>$goods['goods_name']
             ]
         ];
         $datainfo=json_encode($datainfo,JSON_UNESCAPED_UNICODE);
