fb765efcb9e958331063cb922459495f5185794f
yuanshuai <yuanshuai@localhost.localdomain>
2019-05-27 02:50:41 +0000

    添加注释，优化代码

--- app/Http/Controllers/San/SanController.php
+++ app/Http/Controllers/San/SanController.php
@@ -29,45 +29,37 @@ class SanController extends Controller
         $EventKey=$obj->EventKey;
         $Content = $obj->Content;//获取文字内容
         if($MsgType=="event"){
-            if($Event=="subscribe"){
+            if($Event=="subscribe"){//用户关注
                 $text="回答问题哟！！！";
-                $xml="<xml>
-                  <ToUserName><![CDATA[".$FromUserName."]]></ToUserName>
-                  <FromUserName><![CDATA[".$ToUserName."]]></FromUserName>
-                  <CreateTime>".time()."</CreateTime>
-                  <MsgType><![CDATA[text]]></MsgType>
-                  <Content><![CDATA[$text]]></Content>
-                </xml>";
-                return $xml;
-            }elseif($Event=="CLICK"){
-                if($EventKey=="da"){
+                $this->returnText($FromUserName,$ToUserName,$text);
+            }elseif($Event=="CLICK"){//用户点击菜单
+
+                if($EventKey=="da"){//点击回答问题
                     //答题  1、去数据库查取一条数据返回
-                    $data=SanModel::orderByRaw("RAND()")->first()->toArray();
-                    $text=$data['wx_name']."回答A是".$data['wx_a']."or回答B是".$data['wx_b'];
-                    $xml="<xml>
-                  <ToUserName><![CDATA[".$FromUserName."]]></ToUserName>
-                  <FromUserName><![CDATA[".$ToUserName."]]></FromUserName>
-                  <CreateTime>".time()."</CreateTime>
-                  <MsgType><![CDATA[text]]></MsgType>
-                  <Content><![CDATA[$text]]></Content>
-                </xml>";
-                    return $xml;
-                }elseif($EventKey=="cheng"){
-                    //查询FormUserName的成绩单
+                    $data=SanModel::orderByRaw("RAND()")->first()->toArray();//查询题目
+                    $text=$data['wx_name']."回答A是".$data['wx_a']."or回答B是".$data['wx_b'];//让用户回答问题
+                    $this->returnText($FromUserName,$ToUserName,$text);//返回问题
+
+                }elseif($EventKey=="cheng"){//点击查询成绩单
                     $nickname=openId($FromUserName);//获取用户信息
-                    $data=EvilModel::where(['openid'=>$FromUserName])->first();
-                    $text="您好".$nickname['nickname']."。"."您回答正确".$data['wx_cor']."。"."回答错误".$data['wx_cor_no'];
-                    $xml="<xml>
-                  <ToUserName><![CDATA[".$FromUserName."]]></ToUserName>
-                  <FromUserName><![CDATA[".$ToUserName."]]></FromUserName>
-                  <CreateTime>".time()."</CreateTime>
-                  <MsgType><![CDATA[text]]></MsgType>
-                  <Content><![CDATA[$text]]></Content>
-                </xml>";
-                    return $xml;
+                    $data=EvilModel::where(['openid'=>$FromUserName])->first();//根据用户openid查询自己的成绩
+                    $text="您好".$nickname['nickname']."。"."您回答正确".$data['wx_cor']."。"."回答错误".$data['wx_cor_no'];//拼装数据
+                    $this->returnText($FromUserName,$ToUserName,$text);//返回文字信息
                 }
             }
         }
 
+    }
+
+    //回复用户文字消息
+    public function returnText($FromUserName,$ToUserName,$text){
+        $xml="<xml>
+                  <ToUserName><![CDATA[".$FromUserName."]]></ToUserName>
+                  <FromUserName><![CDATA[".$ToUserName."]]></FromUserName>
+                  <CreateTime>".time()."</CreateTime>
+                  <MsgType><![CDATA[text]]></MsgType>
+                  <Content><![CDATA[$text]]></Content>
+                </xml>";
+        return $xml;
     }
 }
